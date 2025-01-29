eec72503db8f88ce45d79d997039ec6622a09aa8
Jarius Miguel Catapang Ballesteros <jrsmglctpngbllstrs@gmail.com>
2025-01-29 10:51:26 +0800

    mali yung initial commit so, eto yung tama

--- pos/forms.py
+++ pos/forms.py
@@ -4,4 +4,4 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['customer_name', 'email', 'address', 'quantity']
+        fields = ['customer_name', 'email', 'address','coffee', 'quantity']
